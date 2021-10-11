fb9441c13d385e2d42d39c32c1657fe0f61a553b
kevinchunyu <kevin11@uw.edu>
2021-10-11 09:49:50 -0700

    updated new section code for practice

--- lab2.R
+++ lab2.R
@@ -9,6 +9,41 @@ b <- 5
 print(paste("A plus B is:", a + b))
 
 # Functions
+
+# Sample conditional #1
+grades <- c(90, 92, 21, 50, 75)
+
+
+# get_class_status <- function(grades) {
+#   if(mean(grades) > 90) {
+#     print("Class is doing good.")
+#   } else {
+#     print("Class is doing not good.")
+#   }
+# }
+# 
+# get_class_status(grades)
+
+
+# Sample conditional #2
+
+# get_even_or_odd <- function(number) {
+#   if (number %% 2 == 0 & number <= 10) {
+#     print("Number is even and less than 10.")
+#   } else {
+#     # we know that the number is odd
+#     if(number < 0) {
+#       print("Number if odd and negative.")
+#     } else {
+#       print("Number is odd and positive.")
+#     }
+#   }
+# }
+# 
+# get_even_or_odd(7)
+
+
+
 # Using mean, median, mode
 list_data <- c(1,2,3,4,5)
 
@@ -30,3 +65,4 @@ print(get_stats(list_data))
 data <- read.csv("info201_sectiondemo/data/countlove.csv")
 
 # 2. View data in console, type assignment code in the file
+# View(data)
