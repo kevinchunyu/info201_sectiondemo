b15b3b671d1a1ad63308ea14aaa09d3b9e1f4139
kevinchunyu <kevin11@uw.edu>
2021-10-18 10:17:18 -0700

    uploaded new lab3 section code

--- lab2.R
+++ lab2.R
@@ -2,12 +2,6 @@
 # Review
 # Variables and Functions, Data
 
-# Declaring variables
-# Can also use different operators ( +, *, %, /)
-a <- 3
-b <- 5
-print(paste("A plus B is:", a + b))
-
 # Logical Operators
 string_var =  "Hello"
 number_var = 7 
@@ -19,47 +13,9 @@ string_var != "Hello"
 
 # Combine operators
 (string_var == "Hello" | number_var == 5) # OR (LEFT OR RIGHT IS TRUE)
-
 (string_var == "Hello" & number_var == 5) # AND
 (string_var == "Hello" & number_var == 7)
 (string_var == "Hello" & bool_var)
-
-
-
-# Functions
-
-# Sample conditional #1
-grades <- c(90, 92, 21, 50, 75)
-
-
-# get_class_status <- function(grades) {
-#   if(mean(grades) > 90) {
-#     print("Class is doing good.")
-#   } else {
-#     print("Class is doing not good.")
-#   }
-# }
-# 
-# get_class_status(grades)
-
-
-# Sample conditional #2
-
-# get_even_or_odd <- function(number) {
-#   if (number %% 2 == 0 & number <= 10) {
-#     print("Number is even and less than 10.")
-#   } else {
-#     # we know that the number is odd
-#     if(number < 0) {
-#       print("Number if odd and negative.")
-#     } else {
-#       print("Number is odd and positive.")
-#     }
-#   }
-# }
-# 
-# get_even_or_odd(7)
-
 
 
 # Using mean, median, mode
@@ -77,6 +33,41 @@ get_stats <- function(list) {
 
 print(get_stats(list_data))
 
+
+# Functions
+
+# Sample conditional #1
+grades <- c(90, 92, 21, 50, 75)
+
+
+get_class_status <- function(grades) {
+  if(mean(grades) > 90) {
+    print("Class is doing good.")
+  } else {
+    print("Class is doing not good.")
+  }
+}
+
+get_class_status(grades)
+
+
+# Sample conditional #2
+get_even_or_odd <- function(number) {
+  if (number %% 2 == 0 & number <= 10) {
+    print("Number is even and less than 10.")
+  } else {
+    # we know that the number is odd
+    if(number < 0) {
+      print("Number if odd and negative.")
+    } else {
+      print("Number is odd and positive.")
+    }
+  }
+}
+
+get_even_or_odd(7)
+
+
 ## Assignment 1: Introduction
 
 # 1. Reading data from a csv file
@@ -88,9 +79,9 @@ data <- read.csv("data/countlove.csv")
 # 3. libraries
 library(stringr)
 # install.packages(stringr) if have not installed a new package
-str_detect("hello", "ello")
-str_count("memes", "e")
 
-# 4. dataframes - accessing columns and reading data
-# data$Date
-# data$Location
+# detects a string pattern of "ello" and returns true if pattern is detected in given string, else false.
+str_detect("hello", "ello")
+
+# counts the number of occurrences of a given character in a string
+str_count("memes", "e")
---
+++ lab3.R
@@ -0,0 +1,47 @@
+# INFO201 LAB 3
+# 10/18/2021
+
+# library 
+library(stringr)
+
+# Dataframes
+data <- read.csv("data/countlove.csv")
+
+# Extracting columns
+location <- data$Location
+
+# unique location
+unique_location <- unique(location)
+print(unique_location)
+
+
+# string detection and how to use it
+str_detect(unique_location, "NY")
+
+# dataframe review/lecture
+class(data['Location']) # VECTOR
+
+class(data[['Location']]) # CHARACTER 
+
+# in general make sure you are familiar with both differences and when to use them
+# Usually if you are graphing/plotting - single brackets - or use the dollar sign to get values
+
+# Data frame manipulation and obtaining data
+
+
+# Obtaining Attendees from data that are equal to exactly 1500
+# Returns a data frame
+# Contains NA values - which generally there are many different ways to ignore/not include these data
+# into our results
+attendees <-data$Attendees
+a <- length(data[attendees == 1500, ])
+a
+
+# a note on dates
+class(data$Date)
+
+dates <- as.Date(data$Date)
+
+class(dates)
+
+
